6c221834483fc00115f8b94ffb22cb981f7ed50d
Ferris <suring-13@foxmail.com>
2025-08-10 17:12:33 +0800

    使用 #[allow(unused_assignments)] 抑制警告

--- src/sliding_window/fixed_length.rs
+++ src/sliding_window/fixed_length.rs
@@ -228,6 +228,7 @@ pub mod n1052 {
     pub fn max_satisfied(customers: Vec<i32>, grumpy: Vec<i32>, minutes: i32) -> i32 {
         let window_size = minutes as usize;
         let mut s = [0, 0]; // s[0] 老板不生气时的顾客数量，s[1] 老板生气时的顾客数量
+        #[allow(unused_assignments)]
         let mut max_s1 = 0;
 
         customers
