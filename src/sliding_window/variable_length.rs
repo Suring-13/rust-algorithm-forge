2da1d0fa26122738cf8ebf76f4896b26abee9f13
Ferris <suring-13@foxmail.com>
2025-08-25 08:46:42 +0800

    76. 最小覆盖子串

--- src/sliding_window/variable_length.rs
+++ src/sliding_window/variable_length.rs
@@ -425,3 +425,106 @@ pub mod n2875 {
         }
     }
 }
+
+// 76. 最小覆盖子串
+pub mod n76 {
+    pub fn min_window(S: String, t: String) -> String {
+        fn is_covered(cnt_s: &[i32; 128], cnt_t: &[i32; 128]) -> bool {
+            for i in b'A'..=b'Z' {
+                if cnt_s[i as usize] < cnt_t[i as usize] {
+                    return false;
+                }
+            }
+            for i in b'a'..=b'z' {
+                if cnt_s[i as usize] < cnt_t[i as usize] {
+                    return false;
+                }
+            }
+            true
+        }
+
+        let mut cnt_s = [0; 128]; // s 子串字母的出现次数
+        let mut cnt_t = [0; 128]; // t 中字母的出现次数
+        for c in t.bytes() {
+            cnt_t[c as usize] += 1;
+        }
+
+        let s = S.as_bytes();
+        let m = s.len();
+        let mut ans_left = 0;
+        let mut ans_right = m;
+
+        let mut left = 0;
+        for (right, &c) in s.iter().enumerate() {
+            // 移动子串右端点
+            cnt_s[c as usize] += 1; // 右端点字母移入子串
+            while is_covered(&cnt_s, &cnt_t) {
+                // 涵盖
+                if right - left < ans_right - ans_left {
+                    // 找到更短的子串
+                    ans_left = left; // 记录此时的左右端点
+                    ans_right = right;
+                }
+                cnt_s[s[left] as usize] -= 1; // 左端点字母移出子串
+                left += 1;
+            }
+        }
+
+        if ans_right < m {
+            S[ans_left..=ans_right].to_string()
+        } else {
+            String::new()
+        }
+    }
+
+    pub fn min_window1(S: String, t: String) -> String {
+        let mut cnt = [0; 128];
+        let mut less = 0;
+        for c in t.bytes() {
+            let c = c as usize;
+            if cnt[c] == 0 {
+                less += 1; // 有 less 种字母的出现次数 < t 中的字母出现次数
+            }
+            cnt[c] += 1;
+        }
+
+        let s = S.as_bytes();
+        let m = s.len();
+        let mut ans_left = 0;
+        let mut ans_right = m;
+
+        let mut left = 0;
+        for (right, &c) in s.iter().enumerate() {
+            // 移动子串右端点
+            let c = c as usize;
+            cnt[c] -= 1; // 右端点字母移入子串
+            if cnt[c] == 0 {
+                // 原来窗口内 c 的出现次数比 t 的少，现在一样多
+                less -= 1;
+            }
+            while less == 0 {
+                // 涵盖：所有字母的出现次数都是 >=
+                if right - left < ans_right - ans_left {
+                    // 找到更短的子串
+                    ans_left = left; // 记录此时的左右端点
+                    ans_right = right;
+                }
+                let x = s[left] as usize; // 左端点字母
+                if cnt[x] == 0 {
+                    // x 移出窗口之前，检查出现次数，
+                    // 如果窗口内 x 的出现次数和 t 一样，
+                    // 那么 x 移出窗口后，窗口内 x 的出现次数比 t 的少
+                    less += 1;
+                }
+                cnt[x] += 1; // 左端点字母移出子串
+                left += 1;
+            }
+        }
+
+        if ans_right < m {
+            S[ans_left..=ans_right].to_string()
+        } else {
+            String::new()
+        }
+    }
+}
