12a218c3a17d999761549479ca7ae77d4a9563a3
Ferris <suring-13@foxmail.com>
2025-09-16 08:39:11 +0800

    344. 反转字符串

--- src/sliding_window/two_pointers.rs
+++ src/sliding_window/two_pointers.rs
@@ -1,1 +1,13 @@
-
+// 344. 反转字符串
+pub mod n344 {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        let n = s.len();
+        let mut left = 0;
+        let mut right = n - 1;
+        while left < right {
+            s.swap(left, right);
+            left += 1;
+            right -= 1;
+        }
+    }
+}
