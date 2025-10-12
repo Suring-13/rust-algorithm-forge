a1a742a5a0054c3db9ac866856bfb11889179374
Ferris <suring-13@foxmail.com>
2025-10-12 18:51:07 +0800

    2972. 统计移除递增子数组的数目 II

--- src/sliding_window/two_pointers.rs
+++ src/sliding_window/two_pointers.rs
@@ -935,3 +935,32 @@ pub mod n1574 {
         ans as _
     }
 }
+
+// 2972. 统计移除递增子数组的数目 II
+pub mod n2972 {
+    pub fn incremovable_subarray_count(nums: Vec<i32>) -> i64 {
+        let n = nums.len();
+        let mut i = 0;
+        while i < n - 1 && nums[i] < nums[i + 1] {
+            i += 1;
+        }
+        if i == n - 1 {
+            // 每个非空子数组都可以移除
+            return n as i64 * (n + 1) as i64 / 2;
+        }
+
+        let mut i = i as i64;
+        let mut ans = i + 2; // 不保留后缀的情况，一共 i+2 个
+        // 枚举保留的后缀为 nums[j:]
+        let mut j = n - 1;
+        while j == n - 1 || nums[j] < nums[j + 1] {
+            while i >= 0 && nums[i as usize] >= nums[j] {
+                i -= 1;
+            }
+            // 可以保留前缀 nums[:i+1], nums[:i], ..., nums[:0] 一共 i+2 个
+            ans += i + 2;
+            j -= 1;
+        }
+        ans
+    }
+}
