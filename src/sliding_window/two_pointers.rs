722cd73de1bf3ba511cc95ea807fa7da1664cfdc
Ferris <suring-13@foxmail.com>
2025-11-04 08:45:28 +0800

    2337. 移动片段得到字符串

--- src/sliding_window/two_pointers.rs
+++ src/sliding_window/two_pointers.rs
@@ -1615,3 +1615,42 @@ pub mod n809 {
             .count() as i32
     }
 }
+
+// 2337. 移动片段得到字符串
+pub mod n2337 {
+    pub fn can_change(start: String, target: String) -> bool {
+        let (s, t) = (start.as_bytes(), target.as_bytes());
+        let (n, m) = (s.len(), t.len());
+        if n != m {
+            return false;
+        }
+
+        let mut i = 0;
+        let mut j = 0;
+
+        while i < n || j < m {
+            while i < n && s[i] == b'_' {
+                i += 1;
+            }
+            while j < m && t[j] == b'_' {
+                j += 1;
+            }
+            if i < n && j < m {
+                if s[i] != t[j] {
+                    return false;
+                }
+                if s[i] == b'L' && i < j {
+                    return false;
+                }
+                if s[i] == b'R' && i > j {
+                    return false;
+                }
+                i += 1;
+                j += 1;
+            } else if i < n || j < m {
+                return false;
+            }
+        }
+        true
+    }
+}
