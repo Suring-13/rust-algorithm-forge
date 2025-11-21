bc8d93f7b7643c8735b5de05737a065317b43f5b
Ferris <suring-13@foxmail.com>
2025-11-21 08:39:23 +0800

    485. 最大连续 1 的个数

--- src/sliding_window/grouped_loop.rs
+++ src/sliding_window/grouped_loop.rs
@@ -1,1 +1,19 @@
+// 485. 最大连续 1 的个数
+pub mod n485 {
+    pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+        let mut ans = 0;
+        let mut cnt = 0; // 存储当前连续1的个数
 
+        // 遍历nums中的每个元素
+        for &x in nums.iter() {
+            if x == 1 {
+                cnt += 1; // 遇到1，当前连续计数+1
+                ans = ans.max(cnt); // 更新最大值
+            } else {
+                cnt = 0; // 遇到0，重置当前连续计数
+            }
+        }
+
+        ans
+    }
+}
