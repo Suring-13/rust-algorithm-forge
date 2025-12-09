3f2915f09e7ce5fcd6abf796ad0d065ceb88fca3
Ferris <suring-13@foxmail.com>
2025-12-09 08:37:22 +0800

    2900. 最长相邻不相等子序列 I

--- src/sliding_window/grouped_loop.rs
+++ src/sliding_window/grouped_loop.rs
@@ -425,3 +425,18 @@ pub mod n2038 {
         cnt[0] > cnt[1]
     }
 }
+
+// 2900. 最长相邻不相等子序列 I
+pub mod n2900 {
+    pub fn get_longest_subsequence(words: Vec<String>, groups: Vec<i32>) -> Vec<String> {
+        let n = groups.len();
+        let mut ans = vec![];
+        for (i, word) in words.into_iter().enumerate() {
+            if i == n - 1 || groups[i] != groups[i + 1] {
+                // i 是连续相同段的末尾
+                ans.push(word);
+            }
+        }
+        ans
+    }
+}
