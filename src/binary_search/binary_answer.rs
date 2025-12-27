6e09ebeeac8cd38d9e14f33d419914dfe8a7f8bc
Ferris <suring-13@foxmail.com>
2025-12-27 16:31:55 +0800

    35. 搜索插入位置

--- src/binary_search/binary_answer.rs
+++ src/binary_search/binary_answer.rs
@@ -35,3 +35,28 @@ pub mod n34 {
         vec![start as i32, end as i32]
     }
 }
+
+// 35. 搜索插入位置
+pub mod n35 {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len(); // 为避免出现负数，使用左闭右开区间 [left, right) 是最方便的
+        while left < right {
+            // 循环不变量：
+            // nums[left-1] < target
+            // nums[right] >= target
+            let mid = left + (right - left) / 2; // 为了防止溢出，不使用 （left + right） / 2
+            if nums[mid] >= target {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        // 循环结束后 left = right
+        // 此时 nums[left-1] < target 而 nums[left] = nums[right] >= target
+        // 所以 lefth/right 就是第一个 >= target 的元素下标
+
+        right as _ // 或者 left，写 right 是为了更好记忆。（right满足什么条件，返回的就是第一个满足该条件的值）
+    }
+}
