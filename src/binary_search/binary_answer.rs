cf86355847576799f7190a109bdededea3219a49
Ferris <suring-13@foxmail.com>
2025-12-28 13:59:34 +0800

    2389. 和有限的最长子序列

--- src/binary_search/binary_answer.rs
+++ src/binary_search/binary_answer.rs
@@ -152,3 +152,52 @@ pub mod n2529 {
         neg.max(pos) as _
     }
 }
+
+// 2389. 和有限的最长子序列
+pub mod n2389 {
+    pub fn answer_queries(nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
+        fn custom_partition_point<T, P>(slice: &[T], mut pred: P) -> usize
+        where
+            P: FnMut(&T) -> bool,
+        {
+            let mut size = slice.len();
+            let (mut left, mut right) = (0, size); // 左闭右开区间 [left, right)
+            while left < right {
+                // 循环不变量：
+                // pred(&slice[left-1]) 为 true
+                // pred(&slice[right]) 为 false
+                let mid = left + size / 2;
+                if pred(&slice[mid]) {
+                    left = mid + 1; // 范围缩小到 [mid+1, right)
+                } else {
+                    right = mid; // 范围缩小到 [left, mid)
+                }
+                size = right - left;
+            }
+
+            // 循环结束后 left = right
+            // 此时 pred(&slice[left-1]) 为 true 而 pred(&slice[right])、pred(&slice[right]) 为 false
+            // 所以 lefth/right 就是第一个 pred 为 false 的元素下标
+
+            right // 或者 left，写 right 是为了更好记忆。（right满足什么条件，返回的就是第一个满足该条件的值）
+        }
+
+        // 1. 对 nums 进行排序
+        let mut sorted_nums = nums;
+        sorted_nums.sort_unstable(); // 不稳定排序，效率更高（若需稳定排序用 sort()）
+
+        // 2. 原地计算前缀和
+        for i in 1..sorted_nums.len() {
+            sorted_nums[i] += sorted_nums[i - 1];
+        }
+
+        // 3. 对每个 query 执行二分查找，生成结果
+        queries
+            .into_iter()
+            .map(|q| {
+                let pos = custom_partition_point(&sorted_nums, |&x| x <= q);
+                pos as i32
+            })
+            .collect()
+    }
+}
