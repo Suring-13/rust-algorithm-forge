abd7ee8d15d5a40a77bca8a5fb122d1e41209bd7
Ferris <suring-13@foxmail.com>
2025-12-31 08:35:38 +0800

    2080. 区间内查询数字的频率

--- src/binary_search/binary_answer.rs
+++ src/binary_search/binary_answer.rs
@@ -249,3 +249,33 @@ pub mod n2300 {
         spells
     }
 }
+
+// 2080. 区间内查询数字的频率
+pub mod n2080 {
+    #![allow(dead_code)]
+    use std::collections::HashMap;
+
+    struct RangeFreqQuery {
+        pos: HashMap<i32, Vec<usize>>,
+    }
+
+    impl RangeFreqQuery {
+        fn new(arr: Vec<i32>) -> Self {
+            let mut pos: HashMap<i32, Vec<usize>> = HashMap::new();
+            for (i, &x) in arr.iter().enumerate() {
+                pos.entry(x).or_default().push(i);
+            }
+            Self { pos }
+        }
+
+        fn query(&self, left: i32, right: i32, value: i32) -> i32 {
+            if let Some(a) = self.pos.get(&value) {
+                let p = a.partition_point(|&i| i < left as usize);
+                let q = a.partition_point(|&i| i <= right as usize);
+                (q - p) as _
+            } else {
+                0
+            }
+        }
+    }
+}
