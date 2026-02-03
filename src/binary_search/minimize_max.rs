30d000aaf8d3d9c25d1e5aea8f99bc3fcf48b46f
Ferris <suring-13@foxmail.com>
2026-02-03 08:49:06 +0800

    2064. 分配给商店的最多商品的最小值

--- src/binary_search/minimize_max.rs
+++ src/binary_search/minimize_max.rs
@@ -31,3 +31,34 @@ pub mod n410 {
         right
     }
 }
+
+// 2064. 分配给商店的最多商品的最小值
+pub mod n2064 {
+    pub fn minimized_maximum(n: i32, quantities: Vec<i32>) -> i32 {
+        let check = |mx: i32| -> bool {
+            let mut cnt = 0;
+            for &q in &quantities {
+                // 等价于 q/mx 向上取整
+                cnt += (q + mx - 1) / mx;
+                // 提前终止
+                if cnt > n {
+                    return true;
+                }
+            }
+            false
+        };
+
+        let mut left = 1; // 最小分配量至少为1
+        let mut right = *quantities.iter().max().unwrap(); // 右边界取最大值
+
+        while left < right {
+            let mid = left + (right - left) / 2;
+            if check(mid) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        right
+    }
+}
