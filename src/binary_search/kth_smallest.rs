a0fff00d1d7279cef6a8446c56457f31c316f0c9
Ferris <suring-13@foxmail.com>
2026-02-22 22:51:17 +0800

    378. 有序矩阵中第 K 小的元素

--- src/binary_search/kth_smallest.rs
+++ src/binary_search/kth_smallest.rs
@@ -37,3 +37,37 @@ pub mod n668 {
         right as i32
     }
 }
+
+// 378. 有序矩阵中第 K 小的元素
+pub mod n378 {
+    pub fn kth_smallest(matrix: Vec<Vec<i32>>, k: i32) -> i32 {
+        let n = matrix.len();
+
+        let check = |mx: i32| -> bool {
+            let mut cnt = 0; // matrix 中的 <= mx 的元素个数
+            let mut i = 0;
+            let mut j = n as i32 - 1; // 从右上角开始
+            while i < n && j >= 0 && cnt < k {
+                if matrix[i][j as usize] > mx {
+                    j -= 1; // 排除第 j 列
+                } else {
+                    cnt += j + 1; // 从 matrix[i][0] 到 matrix[i][j] 都 <= mx
+                    i += 1;
+                }
+            }
+            cnt >= k
+        };
+
+        let mut left = matrix[0][0];
+        let mut right = matrix[n - 1][n - 1];
+        while left < right {
+            let mid = left + (right - left) / 2;
+            if check(mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        right
+    }
+}
