fabb007b4963102c24a196af932850191d603698
Ferris <suring-13@foxmail.com>
2026-03-02 08:48:36 +0800

    3134. 找出唯一性数组的中位数

--- src/binary_search/kth_smallest.rs
+++ src/binary_search/kth_smallest.rs
@@ -428,3 +428,46 @@ pub mod n3116 {
         right
     }
 }
+
+// 3134. 找出唯一性数组的中位数
+pub mod n3134 {
+    pub fn median_of_uniqueness_array(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let k = ((n * (n + 1) / 2 + 1) / 2) as i64;
+
+        let check = |upper: usize| -> bool {
+            let mut cnt = 0i64;
+            let mut l = 0;
+            let mut freq = std::collections::HashMap::new();
+            for (r, &x) in nums.iter().enumerate() {
+                *freq.entry(x).or_insert(0) += 1; // 移入右端点
+                while freq.len() > upper {
+                    // 窗口内元素过多
+                    let e = freq.entry(nums[l]).or_insert(0);
+                    *e -= 1;
+                    if *e == 0 {
+                        freq.remove(&nums[l]); // 移出左端点
+                    }
+                    l += 1;
+                }
+                cnt += (r - l + 1) as i64; // 右端点固定为 r 时，有 r-l+1 个合法左端点
+                if cnt >= k {
+                    return true;
+                }
+            }
+            false
+        };
+
+        let mut left = 0;
+        let mut right = n;
+        while left < right {
+            let mid = left + (right - left) / 2;
+            if check(mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        right as _
+    }
+}
