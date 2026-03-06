e61b9b98958d7beb00b290d817cb9399a538d6b1
Ferris <suring-13@foxmail.com>
2026-03-06 08:48:24 +0800

    1. 两数之和

--- src/data_structures/enum_tricks.rs
+++ src/data_structures/enum_tricks.rs
@@ -1,1 +1,15 @@
-
+// 1. 两数之和
+pub mod n1 {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut idx = std::collections::HashMap::new(); // 创建一个空哈希表
+        // 枚举 j
+        for (j, &x) in nums.iter().enumerate() {
+            // 在左边找 nums[i]，满足 nums[i]+x=target
+            if let Some(&i) = idx.get(&(target - x)) {
+                return vec![i as i32, j as i32]; // 返回两个数的下标
+            }
+            idx.insert(x, j); // 保存 nums[j] 和 j
+        }
+        unreachable!() // 题目保证有解，循环中一定会 return
+    }
+}
