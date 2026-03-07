f0a2e0c28a3b17785a817c181d5e430cdebec44a
Ferris <suring-13@foxmail.com>
2026-03-07 18:04:40 +0800

    1512. 好数对的数目

--- src/data_structures/enum_tricks.rs
+++ src/data_structures/enum_tricks.rs
@@ -13,3 +13,22 @@ pub mod n1 {
         unreachable!() // 题目保证有解，循环中一定会 return
     }
 }
+
+// 1512. 好数对的数目
+pub mod n1512 {
+    pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
+        let mut ans = 0; // 最终答案：好数对总数
+        let mut cnt = std::collections::HashMap::new(); // 记录每个数字已经出现几次
+
+        for x in nums {
+            // 遍历每个数 x（相当于 j 位置）
+            // 拿到 x 之前出现的次数，没有就是 0
+            let e = cnt.entry(x).or_insert(0);
+
+            ans += *e; // 之前有多少个 x，就新增多少个好数对
+            *e += 1; // 再把当前 x 计入计数
+        }
+
+        ans
+    }
+}
