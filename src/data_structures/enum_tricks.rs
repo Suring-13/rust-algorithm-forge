9880b7139d2e835cfccdf0ea98a3fc79522479f6
Ferris <suring-13@foxmail.com>
2026-03-27 23:03:41 +0800

    2905. 找出满足差值条件的下标 II

--- src/data_structures/enum_tricks.rs
+++ src/data_structures/enum_tricks.rs
@@ -441,3 +441,26 @@ pub mod n3584 {
         ans
     }
 }
+
+// 2905. 找出满足差值条件的下标 II
+pub mod n2905 {
+    pub fn find_indices(nums: Vec<i32>, index_difference: i32, value_difference: i32) -> Vec<i32> {
+        let mut max_idx = 0;
+        let mut min_idx = 0;
+        for j in index_difference as usize..nums.len() {
+            let i = j - index_difference as usize;
+            if nums[i] > nums[max_idx] {
+                max_idx = i;
+            } else if nums[i] < nums[min_idx] {
+                min_idx = i;
+            }
+            if nums[max_idx] - nums[j] >= value_difference {
+                return vec![max_idx as i32, j as i32];
+            }
+            if nums[j] - nums[min_idx] >= value_difference {
+                return vec![min_idx as i32, j as i32];
+            }
+        }
+        vec![-1, -1]
+    }
+}
