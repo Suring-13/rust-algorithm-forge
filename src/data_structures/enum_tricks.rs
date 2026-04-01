f909009c80e071a54daa465d26308123c142ce5c
Ferris <suring-13@foxmail.com>
2026-04-01 08:49:42 +0800

    2874. 有序三元组中的最大值 II

--- src/data_structures/enum_tricks.rs
+++ src/data_structures/enum_tricks.rs
@@ -545,3 +545,18 @@ pub mod n2506 {
         ans
     }
 }
+
+// 2874. 有序三元组中的最大值 II
+pub mod n2874 {
+    pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
+        let mut ans = 0;
+        let mut max_diff = 0;
+        let mut pre_max = 0;
+        for x in nums {
+            ans = ans.max(max_diff as i64 * x as i64);
+            max_diff = max_diff.max(pre_max - x);
+            pre_max = pre_max.max(x);
+        }
+        ans
+    }
+}
