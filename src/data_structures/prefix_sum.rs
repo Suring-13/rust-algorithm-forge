65102055bfedd809a21b5c1620cff28a4603d8aa
Ferris <suring-13@foxmail.com>
2026-04-27 12:42:11 +0800

    303. 区域和检索 - 数组不可变

--- src/data_structures/prefix_sum.rs
+++ src/data_structures/prefix_sum.rs
@@ -1,1 +1,25 @@
+// 303. 区域和检索 - 数组不可变
+pub mod n303 {
+    pub struct NumArray {
+        pub pre_sum: Vec<i32>,
+    }
 
+    impl NumArray {
+        pub fn new(nums: Vec<i32>) -> Self {
+            let mut pre_sum = vec![0; nums.len() + 1];
+            // pre_sum[0] = 0
+            // pre_sum[i] = nums[0] + nums[1] + ... + nums[i-1]
+            for (i, &num) in nums.iter().enumerate() {
+                pre_sum[i + 1] = pre_sum[i] + num;
+            }
+            Self { pre_sum }
+        }
+
+        // [left, right] 区间和
+        pub fn sum_range(&self, left: i32, right: i32) -> i32 {
+            let l = left as usize;
+            let r = right as usize;
+            self.pre_sum[r + 1] - self.pre_sum[l]
+        }
+    }
+}
