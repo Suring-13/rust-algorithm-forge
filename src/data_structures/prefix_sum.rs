394a61cf42d6a113f311490e0fc0d24c969100c9
Ferris <suring-13@foxmail.com>
2026-05-03 17:06:44 +0800

    53. 最大子数组和

--- src/data_structures/prefix_sum.rs
+++ src/data_structures/prefix_sum.rs
@@ -124,3 +124,18 @@ pub mod n1749 {
         mx - mn
     }
 }
+
+// 53. 最大子数组和
+pub mod n53 {
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        let mut ans = i32::MIN;
+        let mut min_pre_sum = 0;
+        let mut pre_sum = 0;
+        for x in nums {
+            pre_sum += x; // 当前的前缀和
+            ans = ans.max(pre_sum - min_pre_sum); // 减去前缀和的最小值
+            min_pre_sum = min_pre_sum.min(pre_sum); // 维护前缀和的最小值
+        }
+        ans
+    }
+}
