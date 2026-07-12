c2b5295b5007a1d44d769f0c1e5b3fac83968886
Ferris <suring-13@foxmail.com>
2026-07-12 20:41:33 +0800

    1094. 拼车

--- src/data_structures/prefix_sum.rs
+++ src/data_structures/prefix_sum.rs
@@ -2689,3 +2689,26 @@ pub mod n2960 {
         dec
     }
 }
+
+// 1094. 拼车
+pub mod n1094 {
+    pub fn car_pooling(trips: Vec<Vec<i32>>, capacity: i32) -> bool {
+        let mut d = vec![0; 1001];
+        for t in trips {
+            let num = t[0];
+            let from = t[1] as usize;
+            let to = t[2] as usize;
+            d[from] += num;
+            d[to] -= num;
+        }
+
+        let mut s = 0;
+        for v in d {
+            s += v;
+            if s > capacity {
+                return false;
+            }
+        }
+        true
+    }
+}
