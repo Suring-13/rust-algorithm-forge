fc5a89b45b7aa4f5db6dc243efcf7141c0ae5496
Ferris <suring-13@foxmail.com>
2026-08-11 10:20:25 +0800

    1441. 用栈操作构建数组

--- src/data_structures/stack_queue.rs
+++ src/data_structures/stack_queue.rs
@@ -1,1 +1,17 @@
-
+// 1441. 用栈操作构建数组
+pub mod n1441 {
+    pub fn build_array(target: Vec<i32>, _n: i32) -> Vec<String> {
+        let mut res = Vec::new();
+        let mut cur = 1;
+        for num in target {
+            while cur < num {
+                res.push("Push".into());
+                res.push("Pop".into());
+                cur += 1;
+            }
+            res.push("Push".into());
+            cur += 1;
+        }
+        res
+    }
+}
