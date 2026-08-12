9b60b4ba0452a0488253cd9a94a536751f4026f7
Ferris <suring-13@foxmail.com>
2026-08-12 12:35:34 +0800

    844. 比较含退格的字符串

--- src/data_structures/stack_queue.rs
+++ src/data_structures/stack_queue.rs
@@ -15,3 +15,22 @@ pub mod n1441 {
         res
     }
 }
+
+// 844. 比较含退格的字符串
+pub mod n844 {
+    pub fn backspace_compare(s: String, t: String) -> bool {
+        fn build(s: &str) -> String {
+            let mut ret = Vec::new();
+            for ch in s.chars() {
+                if ch != '#' {
+                    ret.push(ch);
+                } else if !ret.is_empty() {
+                    ret.pop();
+                }
+            }
+            ret.into_iter().collect()
+        }
+
+        build(&s) == build(&t)
+    }
+}
