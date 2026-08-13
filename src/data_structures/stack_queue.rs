c2bdf5e18876fbb6d0a01773d1c45d3c7fed3e1d
Ferris <suring-13@foxmail.com>
2026-08-13 12:31:38 +0800

    682. 棒球比赛

--- src/data_structures/stack_queue.rs
+++ src/data_structures/stack_queue.rs
@@ -34,3 +34,21 @@ pub mod n844 {
         build(&s) == build(&t)
     }
 }
+
+// 682. 棒球比赛
+pub mod n682 {
+    pub fn cal_points(operations: Vec<String>) -> i32 {
+        let mut st = vec![];
+        for op in operations {
+            match op.as_bytes()[0] {
+                b'+' => st.push(st[st.len() - 2] + st[st.len() - 1]),
+                b'D' => st.push(st[st.len() - 1] * 2),
+                b'C' => {
+                    st.pop();
+                }
+                _ => st.push(op.parse::<i32>().unwrap()),
+            }
+        }
+        st.iter().sum()
+    }
+}
